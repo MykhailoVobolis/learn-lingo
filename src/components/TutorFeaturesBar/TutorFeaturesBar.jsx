a98305fe97b6a832d354722929c68b7290c869c4
MykhailoVobolis <mvobolis@gmail.com>
2024-09-27 13:59:21 +0300

    added home page

--- src/components/TutorFeaturesBar/TutorFeaturesBar.jsx
+++ src/components/TutorFeaturesBar/TutorFeaturesBar.jsx
@@ -1,5 +1,27 @@
 import css from "./TutorFeaturesBar.module.css";
 
 export function TutorFeaturesBar() {
-  return <div className={css.TutorFeaturesContainer}></div>;
+  return (
+    <ul className={css.TutorFeaturesContainer}>
+      <svg className={css.borderSvg} xmlns="http://www.w3.org/2000/svg">
+        <rect className={css.borderRect} x="0" y="0" width="100%" height="100%" rx="30" ry="30" />
+      </svg>
+      <li className={css.featurItem}>
+        <p className={css.featurValue}>32,000 +</p>
+        <p className={css.featurName}>Experienced tutors</p>
+      </li>
+      <li className={css.featurItem}>
+        <p className={css.featurValue}>300,000 +</p>
+        <p className={css.featurName}>5-star tutor reviews</p>
+      </li>
+      <li className={css.featurItem}>
+        <p className={css.featurValue}>120 +</p>
+        <p className={css.featurName}>Subjects taught</p>
+      </li>
+      <li className={css.featurItem}>
+        <p className={css.featurValue}>200 +</p>
+        <p className={css.featurName}>Tutor nationalities</p>
+      </li>
+    </ul>
+  );
 }
