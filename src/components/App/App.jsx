f24fa389cb05097bb0963f9aac740f3323f84f9d
MykhailoVobolis <mvobolis@gmail.com>
2024-09-27 14:52:07 +0300

    added not found page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,12 +4,16 @@ import { lazy } from "react";
 import css from "./App.module.css";
 
 const HomePage = lazy(() => import("../../pages/HomePage/HomePage.jsx"));
+const TeachersPage = lazy(() => import("../../pages/TeachersPage/Teachers.jsx"));
+const NotFoundPage = lazy(() => import("../../pages/NotFoundPage/NotFoundPage.jsx"));
 
 export default function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/teachers" element={<TeachersPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Layout>
   );
---
+++ src/pages/TeachersPage/Teachers.jsx
@@ -0,0 +1,5 @@
+import css from "./Teachers.module.css";
+
+export default function TeachersPage() {
+  return <h1>Teachers Page</h1>;
+}
